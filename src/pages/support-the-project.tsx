037242fb4c09cfd342aac9187279081ceca82d11
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2023-05-01 03:33:07 +0500

    Update support-the-project.tsx

--- src/pages/support-the-project.tsx
+++ src/pages/support-the-project.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import GlobalLayout from '../components/layouts/GlobalLayout'
 import { SEO } from '../components/shared/SEO'
+import { useRouter } from 'next/router'
 
 const SupportTheProjectPage: React.FC<any> = () => {
    var disablePage = process.env.NEXT_PUBLIC_DISABLE_PAGE;
+ const router=useRouter();
    if(disablePage === 'true')
     {
-        return null;
+        router.push("/")
     }
   return (
     <div>
