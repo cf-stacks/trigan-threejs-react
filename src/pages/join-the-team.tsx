fe7feaaac2011adc9485a337060b6bfb7299f274
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2023-05-01 03:28:37 +0500

    Update join-the-team.tsx

--- src/pages/join-the-team.tsx
+++ src/pages/join-the-team.tsx
@@ -4,12 +4,14 @@ import 'react-lazy-load-image-component/src/effects/blur.css'
 import GlobalLayout from '../components/layouts/GlobalLayout'
 import { SEO } from '../components/shared/SEO'
 import JoinTheTeamSlider from '../components/home/JoinTheTeamSlider'
+import { useRouter } from 'next/router'
 
 const VisionPage: React.FC<any> = () => {
  var disablePage = process.env.NEXT_PUBLIC_DISABLE_PAGE;
+ const router=useRouter();
    if(disablePage === 'true')
     {
-        return null;
+        router.push("/")
     }
   return (
     <div>
