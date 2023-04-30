d6c0f404dc94639e1780f73357e54f6c55976c36
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2023-05-01 03:24:41 +0500

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,13 +2,16 @@ import AboutComponent from '../components/about'
 import { SEO } from '../components/shared/SEO'
 import React, { lazy, Suspense } from 'react'
 import GlobalLayout from '../components/layouts/GlobalLayout';
+import { Router, useRouter } from 'next/router';
 
 const About = () => {
   var disablePage = process.env.NEXT_PUBLIC_DISABLE_PAGE;
+  const router = useRouter()
    if(disablePage === 'true')
     {
-        return null;
+        return router.push("/") ;
     }
+  
   return (
     <div className='dark:bg-white'>
       <SEO title="About" description='About Trigan' />
