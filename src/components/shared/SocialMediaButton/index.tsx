87e5ba467de0393f8ce0a8e9877f8ce180e9cc99
2bitCode <sachinb1377@gmail.com>
2023-05-07 09:31:41 +0545

    fixed share button

--- src/components/shared/SocialMediaButton/index.tsx
+++ src/components/shared/SocialMediaButton/index.tsx
@@ -16,16 +16,15 @@ const SocialMediaLinks = () => {
   }
 
   function shareOnFacebook(e: any) {
-    e.stopPropagation()
+    // e.stopPropagation()
     const url =
       'https://www.facebook.com/sharer.php?u=' +
       encodeURIComponent(window.location.href)
     window.open(url, '_blank')
-    // return false
   }
 
   function shareOnInstagram(e: any) {
-    e.stopPropagation()
+    // e.stopPropagation()
     const url =
       'https://www.instagram.com/share?url=' +
       encodeURIComponent(window.location.href)
@@ -33,7 +32,7 @@ const SocialMediaLinks = () => {
   }
 
   function shareOnTwitter(e: any) {
-    e.stopPropagation()
+    // e.stopPropagation()
     const url =
       'https://twitter.com/share?url=' +
       encodeURIComponent(window.location.href)
@@ -41,7 +40,7 @@ const SocialMediaLinks = () => {
   }
 
   function shareOnLinkedIn(e: any) {
-    e.stopPropagation()
+    // e.stopPropagation()
     const url =
       'https://www.linkedin.com/sharing/share-offsite/?url=' +
       encodeURIComponent(window.location.href)
@@ -61,7 +60,6 @@ const SocialMediaLinks = () => {
         onClick={(e) => toggleSocialMediaContainer(e)}
       >
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
-          {/* <i className="fas fa-plus text-white"></i> */}
           <FaPlus className="text-white" />
         </div>
         <div
@@ -71,47 +69,43 @@ const SocialMediaLinks = () => {
           )}
           onClick={(e) => shareOnFacebook(e)}
         >
-          {/* <i className="fab fa-facebook-f text-white"></i> */}
           <FaFacebookF size={24} className="text-white" />
         </div>
         <div
           className={classNames(
-            'absolute bottom-0 right-0 flex h-12 w-12 items-center justify-center rounded-full border border-white bg-[#653394] opacity-0 transition-all duration-500',
+            'absolute bottom-0 right-0 flex h-12 w-12 items-center justify-center rounded-full border border-white bg-[#653394] opacity-0 transition-all duration-500 hover:cursor-pointer',
             { 'opacity-100': isOpen }
           )}
           onClick={(e) => shareOnInstagram(e)}
         >
-          {/* <i className="fab fa-twitter text-white"></i> */}
           <FaInstagram size={24} className="text-white" />
         </div>
         <div
           className={classNames(
-            'absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-full border border-white bg-[#653394] opacity-0 transition-all duration-500',
+            'absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-full border border-white bg-[#653394] opacity-0 transition-all duration-500 hover:cursor-pointer',
             { 'opacity-100': isOpen }
           )}
           onClick={(e) => shareOnLinkedIn(e)}
         >
-          {/* <i className="fab fa-instagram text-white"></i> */}
           <FaLinkedin size={24} className="text-white" />
         </div>
         <div
           className={classNames(
-            'absolute bottom-0 left-0 flex h-12 w-12 items-center justify-center rounded-full border border-white bg-[#653394] opacity-0 transition-all duration-500',
+            'absolute bottom-0 left-0 flex h-12 w-12 items-center justify-center rounded-full border border-white bg-[#653394] opacity-0 transition-all duration-500 hover:cursor-pointer',
             { 'opacity-100': isOpen }
           )}
           onClick={(e) => shareOnTwitter(e)}
         >
-          {/* <i className="fab fa-linkedin-in text-white"></i> */}
           <FaTwitter size={24} className="text-white" />
         </div>
       </div>
-      {/* <div
+      <div
         className={classNames(
-          'fixed left-0 top-0 h-full w-full transition-all duration-500'
-          // { 'bg-opacity-50': isOpen }
+          'fixed left-0 top-0 h-full w-full transition-all duration-500',
+          { hidden: isOpen }
         )}
         onClick={toggleSocialMediaContainer}
-      ></div> */}
+      ></div>
     </div>
   )
 }
